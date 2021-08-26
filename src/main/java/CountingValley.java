3b3fa3ed4916e207f660e08e1df18dc75463d360
WebSamurai <priyanshuparate@yahoo.in>
2021-08-26 21:51:12 +0530

    optimize in the valleycounter

--- src/main/java/CountingValley.java
+++ src/main/java/CountingValley.java
@@ -2,24 +2,21 @@ import java.util.stream.Stream;
 
 public class CountingValley {
 
-    public static int countingValleys( String path) {
+    public static int countingValleys(String path) {
         // Write your code here
-        int valllyCouter=0;
-        int initialposition=0;
-        boolean inthevaly=false;
+        int valllyCouter = 0;
+        int initialposition = 0;
+        boolean inthevaly = false;
 
-        for(char c:path.toCharArray()){
-            initialposition= c=='D'?initialposition-1: initialposition+1;
-            if(inthevaly && initialposition<0){
-                    inthevaly=true;
+        for (char c : path.toCharArray()) {
+            initialposition = c == 'D' ? initialposition - 1 : initialposition + 1;
 
-            }
-            else if(inthevaly && initialposition==0){
+            if (inthevaly && initialposition == 0) {
 
                 valllyCouter++;
             }
-           inthevaly=initialposition<0;
+            inthevaly = initialposition < 0;
         }
-        return  valllyCouter;
+        return valllyCouter;
     }
 }
